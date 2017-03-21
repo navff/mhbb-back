ea81ffe66d8db449b9c14770c6c3978901ba8019
navff <var@33kita.ru>
2017-03-21 16:08:53 +0300

    #2 Добавил Avtivity, но не до конца

---
+++ Models/Entities/Activity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace Models.Entities
+{
+    public class Activity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("Organizer")]
+        public int OrganizerId { get; set; }
+        public virtual Organizer Organizer { get; set; }
+
+        public int AgeFrom { get; set; }
+
+        public int AgeTo { get; set; }
+
+        public string Phones { get; set; }
+
+        public StringInfo Address { get; set; }
+
+        public string Prices { get; set; }
+
+        public bool Sobriety { get; set; }
+
+        public string Mentor { get; set; }
+
+        public string Description { get; set; }
+
+        //========================================
+        public int GetRaiting()
+        {
+            //TODO: для рейтинга использовать отдельную табличку. ActivityUsersVoices
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Models/Entities/Organizer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    public class Organizer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [ForeignKey("City")]
+        public int CityId { get; set; }
+        public virtual City City { get; set; }
+    }
+}
