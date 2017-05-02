14b8e1bd15fe807897430fd653a50cb806dcd23f
navff <var@33kita.ru>
2017-05-02 08:50:38 +0300

    #4 Сделан сервисный слой для активностей. Без поиска.

---
+++ Models/Operations/ActivityOperations.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Camps.Tools;
+using Models.Entities;
+using System.Data.Entity;
+
+namespace Models.Operations
+{
+    public class ActivityOperations
+    {
+        private HobbyContext _context;
+
+        public ActivityOperations(HobbyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Activity> GetAsync(int id)
+        {
+            try
+            {
+                return await _context.Activities.Include(a => a.Organizer).FirstOrDefaultAsync(a => a.Id == id);
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT GET ACTIVITY", ex);
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<Activity>> SearchAsync(String word=null, 
+                                                             int? age = null, 
+                                                             int?[] interestIds = null, 
+                                                             int? cityId = null, 
+                                                             bool? sobriety = null, 
+                                                             bool? free = null, 
+                                                             int? page = null)
+        {
+            try
+            {
+                // TODO: сделать нормальный поиск
+                return _context.Activities.ToList();
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT FIND ACTIVITIES", ex);
+                throw;
+            }
+        }
+
+        public async Task<Activity> AddAsync(Activity activity)
+        {
+            try
+            {
+                _context.Activities.Add(activity);
+                await _context.SaveChangesAsync();
+                return activity;
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT ADD ACTIVITY", ex);
+                throw;
+            }
+        }
+
+        public async Task<Activity> UpdateAsync(Activity activity)
+        {
+            try
+            {
+                var activityInDb = _context.Activities.Find(activity.Id);
+                if (activityInDb == null)
+                {
+                    throw new ArgumentException("There is no activity with this Id");
+                }
+                activityInDb.Name = activity.Name;
+                activityInDb.Address = activity.Address;
+                activityInDb.InterestId = activity.InterestId;
+                activityInDb.AgeFrom = activity.AgeFrom;
+                activityInDb.AgeTo = activity.AgeTo;
+                activityInDb.Description = activity.Description;
+                activityInDb.IsChecked = activity.IsChecked;
+                activityInDb.Mentor = activity.Mentor;
+                activityInDb.OrganizerId = activity.OrganizerId;
+                activityInDb.Phones = activity.Phones;
+                activityInDb.Prices = activity.Prices;
+
+                await _context.SaveChangesAsync();
+                return activityInDb;
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT UPDATE ACTIVITY", ex);
+                throw;
+            }
+        }
+
+        public async Task DeleteAsync(int activityId)
+        {
+            try
+            {
+                var activity = _context.Activities.Find(activityId);
+                if (activity == null)
+                {
+                    throw new ArgumentException("There is no activity with this Id");
+                }
+                _context.Activities.Remove(activity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT DELETE ACTIVITY", ex);
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ Tests/Operations/ActivityOperationsTest.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using Models.Entities;
+using Models.Operations;
+using System.Data.Entity;
+
+namespace Tests.Operations
+{
+    [TestClass]
+    public class ActivityOperationsTest : BaseTest
+    {
+        private ActivityOperations _activityOperations;
+
+        public ActivityOperationsTest()
+        {
+            _activityOperations = new ActivityOperations(_context);
+        }
+
+        [TestMethod]
+        public void Get_Ok_Test()
+        {
+            var activity = _context.Activities.First();
+            var result = _activityOperations.GetAsync(activity.Id).Result;
+            Assert.AreEqual(activity.Name, result.Name);
+        }
+
+        [TestMethod]
+        public void Get_WrongId_Test()
+        {
+            var result = _activityOperations.GetAsync(9999).Result;
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void SearchNoParameters_Ok_Test()
+        {
+            var result = _activityOperations.SearchAsync(null, null, null, null, null, null, null).Result;
+            Assert.IsTrue(result.Any());
+        }
+
+        [TestMethod]
+        public void SearchByWord_Ok_Test()
+        {
+            var activity = _context.Activities.First();
+            var result = _activityOperations.SearchAsync(word: activity.Name.Substring(2)).Result.ToList();
+            Assert.IsTrue(result.Any());
+            Assert.AreEqual(activity.Name, result.First().Name);
+        }
+
+
+        [TestMethod]
+        public void Search_Pagination_Test()
+        {
+            var result = _activityOperations.SearchAsync(page: 2).Result;
+            Assert.IsTrue(result.Any());
+        }
+
+        [TestMethod]
+        public void SearchByInterest_Ok_Test()
+        {
+            var activity = _context.Activities.First();
+            var result = _activityOperations.SearchAsync(interestIds: new [] {activity.InterestId}).Result;
+            Assert.IsTrue(result.Any());
+
+        }
+
+        [TestMethod]
+        public void SearchBySobriety_Ok_Test()
+        {
+            var result = _activityOperations.SearchAsync(sobriety: true).Result;
+            Assert.IsTrue(result.Any());
+        }
+
+        [TestMethod]
+        public void SearchByAllFields_Ok_Test()
+        {
+            var activity = _context.Activities.Include(a => a.Organizer).First();
+            var result = _activityOperations.SearchAsync(word: activity.Name.Substring(2),
+                                                         age:10, 
+                                                         interestIds: new[] { activity.InterestId },
+                                                         cityId: activity.Organizer.CityId,
+                                                         sobriety:true,
+                                                         free:true,
+                                                         page:1).Result;
+            Assert.IsTrue(result.Any());
+        }
+
+        [TestMethod]
+        public void Add_Ok_Test()
+        {
+            var rndString = Guid.NewGuid().ToString();
+            var interest = _context.Interests.First();
+            var city = _context.Cities.First();
+            var result = _activityOperations.AddAsync(new Activity()
+            {
+                Name = rndString,
+                Address = rndString,
+                AgeFrom = 1,
+                AgeTo = 20,
+                Description = rndString,
+                InterestId = interest.Id,
+                IsChecked = false,
+                Mentor = rndString,
+                Organizer = new Organizer
+                {
+                    Name = rndString,
+                    CityId = city.Id,
+                    Sobriety = true
+                },
+                Phones = rndString,
+                Prices = rndString
+            }).Result;
+
+            Assert.AreEqual(rndString, result.Organizer.Name);
+            Assert.AreEqual(rndString, result.Address);
+        }
+
+        [TestMethod]
+        public void Update_Ok_Test()
+        {
+            var rndString = Guid.NewGuid().ToString();
+            var activity = _context.Activities.First();
+            activity.Name = rndString;
+
+            var result = _activityOperations.UpdateAsync(activity).Result;
+            Assert.AreEqual(rndString, result.Name);
+        }
+
+        [TestMethod]
+        public void Delete_Ok_Test()
+        {
+            var activity = _context.Activities.Take(10).ToList().Last();
+            _activityOperations.DeleteAsync(activity.Id).Wait();
+
+            using (var cntxt = new HobbyContext())
+            {
+                Assert.IsNull(cntxt.Activities.FirstOrDefault(a => a.Id == activity.Id));
+            }
+        }
+    }
+}
