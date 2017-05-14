d226367e9527603a3626f2bc47224d58940413f9
navff <var@33kita.ru>
2017-05-14 07:15:10 +0300

    #5 Сервисный слой голосований

---
+++ Models/Operations/VoiceOperations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Entities;
+
+namespace Models.Operations
+{
+    public class VoiceOperations
+    {
+        private HobbyContext _context;
+
+        public VoiceOperations(HobbyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActivityUserVoice> AddVoice(string email, VoiceValue voiceValue, int activityId)
+        {
+            var voice = _context.ActivityUserVoices
+                            .FirstOrDefault(v => (v.ActivityId == activityId)
+                                                 && (v.UserEmail == email)) 
+                        ?? new ActivityUserVoice
+                        {
+                            ActivityId = activityId,
+                            UserEmail = email,
+                        };
+            voice.VoiceValue = voiceValue;
+            if (voice.Id == 0)
+            {
+                _context.ActivityUserVoices.Add(voice);
+            }
+            await _context.SaveChangesAsync();
+            return voice;
+        }
+    }
+}
---
+++ Tests/Operations/VoiceOperationsTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models.Entities;
+using Models.Operations;
+
+namespace Tests.Operations
+{
+    [TestClass]
+    public class VoiceOperationsTest : BaseTest
+    {
+        private VoiceOperations _voiceOperations;
+
+        public VoiceOperationsTest()
+        {
+            _voiceOperations = new VoiceOperations(_context);
+        }
+
+        [TestMethod]
+        public void Add_Positive_Test()
+        {
+            var user = _context.Users.First();
+            var activity = _context.Activities.First();
+            var result = _voiceOperations.AddVoice(user.Email, VoiceValue.Positive, activity.Id).Result;
+            Assert.AreEqual(user.Email, result.UserEmail);
+            Assert.AreEqual(activity.Id, result.ActivityId);
+            Assert.AreEqual(VoiceValue.Positive, result.VoiceValue);
+        }
+
+        [TestMethod]
+        public void Add_Negative_Test()
+        {
+            var user = _context.Users.First();
+            var activity = _context.Activities.First();
+            var result = _voiceOperations.AddVoice(user.Email, VoiceValue.Negative, activity.Id).Result;
+            Assert.AreEqual(user.Email, result.UserEmail);
+            Assert.AreEqual(activity.Id, result.ActivityId);
+            Assert.AreEqual(VoiceValue.Negative, result.VoiceValue);
+        }
+
+        
+
+    }
+}
