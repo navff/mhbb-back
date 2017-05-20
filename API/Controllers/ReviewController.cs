b7bbf49b64802decff1820a8546cbf5afe08a4e4
navff <var@33kita.ru>
2017-05-20 13:16:55 +0300

    #11 Начат ReviewController

--- API/Controllers/ReviewController.cs
+++ API/Controllers/ReviewController.cs
@@ -4,34 +4,129 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Description;
+using API.Common;
+using API.Models;
+using API.ViewModels;
+using Camps.Tools;
 
 namespace API.Controllers
 {
+    [RoutePrefix("api/review")]
     public class ReviewController : ApiController
     {
+
+        [HttpGet]
+        [ResponseType(typeof(ReviewViewModelGet))]
         public async Task<IHttpActionResult> Get(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT GET REVIEW", ex);
+                throw;
+            }
         }
 
+        [HttpGet]
+        [Route("byuser")]
+        [ResponseType(typeof(IEnumerable<ReviewViewModelGet>))]
+        public async Task<IHttpActionResult> GetByUser(string email)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT GET REVIEW", ex);
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("byactivity")]
+        [ResponseType(typeof(IEnumerable<ReviewViewModelGet>))]
+        public async Task<IHttpActionResult> GetByActivity(int activityId)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT GET REVIEWS", ex);
+                throw;
+            }
+        }
+
+        [HttpPut]
+        [RESTAuthorize]
+        [ResponseType(typeof(ReviewViewModelGet))]
         public async Task<IHttpActionResult> Put(int id, object putViewModel)
         {
-            throw new NotImplementedException();
+            //TODO: проверить, есть ли права на отзыв
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT PUT REVIEW", ex);
+                throw;
+            }
         }
 
+        [HttpPost]
+        [ResponseType(typeof(ReviewViewModelGet))]
+        [RESTAuthorize]
         public async Task<IHttpActionResult> Post(object postViewModel)
         {
-            throw new NotImplementedException();
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT POST REVIEW", ex);
+                throw;
+            }
         }
 
+        [HttpDelete]
+        [RESTAuthorize]
         public async Task<IHttpActionResult> Delete(int id)
         {
-            throw new NotImplementedException();
+            //TODO: проверить, есть ли права на отзыв
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT DELETE REVIEW", ex);
+                throw;
+            }
         }
 
-        public async Task<IHttpActionResult> SetChecked(int id, bool isChecked = true)
+        [HttpPut]
+        [ResponseType(typeof(ReviewViewModelGet))]
+        [RESTAuthorize(Role.PortalAdmin, Role.PortalManager)]
+        [Route("setchecked")]
+        public async Task<IHttpActionResult> SetChecked(int reviewId, bool isChecked)
         {
-            throw new NotImplementedException();
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.Log("CANNOT SET CHECKED ON REVIEW", ex);
+                throw;
+            }
         }
     }
 }
